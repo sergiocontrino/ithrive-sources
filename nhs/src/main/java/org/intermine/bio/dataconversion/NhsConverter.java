8a8669c91df83426ab471569991994eacd887bbf
sergiocontrino <contrino.it@gmail.com>
2020-07-08 17:04:20 +0100

    ready for multiple files

--- nhs/src/main/java/org/intermine/bio/dataconversion/NhsConverter.java
+++ nhs/src/main/java/org/intermine/bio/dataconversion/NhsConverter.java
@@ -17,16 +17,16 @@ import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.Reader;
 import java.util.*;
 
 
 /**
- * 
  * @author
  */
-public class NhsConverter extends BioFileConverter
-{
+public class NhsConverter extends BioFileConverter {
     //
     private static final String DATASET_TITLE = "Districts reports";
     private static final String DATA_SOURCE_NAME = "NHS";
@@ -37,19 +37,52 @@ public class NhsConverter extends BioFileConverter
 
     /**
      * Constructor
+     *
      * @param writer the ItemWriter used to handle the resultant items
-     * @param model the Model
+     * @param model  the Model
      */
     public NhsConverter(ItemWriter writer, Model model) {
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE, null);
     }
 
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
+    @Override
     public void process(Reader reader) throws Exception {
+        File f = getCurrentFile();
+        String fileName = getCurrentFile().getName();
+        if (fileName.endsWith("csv")) {
+            LOG.info("Reading file: " + fileName);
+            if (fileName.equalsIgnoreCase("campet.csv"))
+                processDemographic(new FileReader(f));
+            if (fileName.equalsIgnoreCase("campetPatLevDia.csv"))
+                processDiagnosis(new FileReader(f));
+        }
+    }
+
+
+
+//    public void process(File dataDir) throws Exception {
+//        List<File> files = readFilesInDir(dataDir);
+//
+//        for (File f : files) {
+//            String fileName = f.getName();
+//            if (fileName.endsWith("csv")) {
+//                LOG.info("Reading file: " + fileName);
+//                if (fileName.equalsIgnoreCase("campet.csv"))
+//                processFile(new FileReader(f));
+//            }
+//        }
+//    }
+
+//    private List<File> readFilesInDir(File dir) {
+//        List<File> files = new ArrayList<File>();
+//        for (File file : dir.listFiles()) {
+//            files.add(file);
+//        }
+//        return files;
+//    }
+
+    private void processDemographic(Reader reader) throws Exception {
+//        public void process(Reader reader) throws Exception {
 
         Set<String> duplicateEnsembls = new HashSet<String>();
         Map<String, Integer> storedGeneIds = new HashMap<String, Integer>();
@@ -67,7 +100,7 @@ public class NhsConverter extends BioFileConverter
 
         // parse header in case
         String[] header = (String[]) lineIter.next();
-        LOG.info("ZZ " + header.toString());
+        LOG.info("DEMO " + Arrays.toString(header));
 
         while (lineIter.hasNext()) {
             String[] line = (String[]) lineIter.next();
@@ -84,12 +117,7 @@ public class NhsConverter extends BioFileConverter
 
             LOG.info("PAT: " + patientId);
 
-            Item patient = createPatient(patientId,ethnicity,gender);
-//            Item patient = createItem("Patient");
-//            //patient.setReference("organism", getOrganism(HUMAN_TAXON));
-//            patient.setAttribute("identifier", patientId);
-//            patient.setAttribute("ethnicity", ethnicity);
-//            patient.setAttribute("gender", gender);
+            Item patient = createPatient(patientId, ethnicity, gender);
 
             Item referral = createItem("Referral");
             referral.setAttribute("patientAge", age);
@@ -105,6 +133,53 @@ public class NhsConverter extends BioFileConverter
         }
     }
 
+
+    private void processDiagnosis(Reader reader) throws Exception {
+//        public void process(Reader reader) throws Exception {
+
+
+        // Read all lines into id pairs, track any ensembl ids or symbols that appear twice
+        Iterator lineIter = FormattedTextParser.parseCsvDelimitedReader(reader);
+
+        // format assumption:
+        // Period,Patient ID,Referral ID,Age at date of referral,EthnicityDescription,Gender
+        // e.g.
+        // 2015-04-01-2019-03-31,1021297,4,17,White - British,M
+        // 2015-04-01-2019-03-31,1098489,3,16,White - British,M
+        // 015-04-01-2019-03-31,1108035,4,13,White - Irish,M
+
+        // parse header in case
+        String[] header = (String[]) lineIter.next();
+        LOG.info("DIA " + Arrays.toString(header));
+/*
+        while (lineIter.hasNext()) {
+            String[] line = (String[]) lineIter.next();
+
+            String period = line[0];
+            String patientId = line[1];
+            String referralId = line[2];
+            String age = line[3];
+            String ethnicity = line[4];
+            String gender = line[5];
+
+            LOG.info("PAT: " + patientId);
+
+            Item patient = createPatient(patientId, ethnicity, gender);
+
+            Item referral = createItem("Referral");
+            referral.setAttribute("patientAge", age);
+            referral.setAttribute("identifier", referralId);
+            referral.setReference("patient", patient);
+
+            //store(patient);
+            store(referral);
+        }
+
+        for (Item item : patients.values()) {
+            Integer pid = store(item);
+        }
+*/
+    }
 
 
     private Item createPatient(String patientId, String ethnicity, String gender)
@@ -123,8 +198,6 @@ public class NhsConverter extends BioFileConverter
         }
         return item;
     }
-
-
 
 
 }
