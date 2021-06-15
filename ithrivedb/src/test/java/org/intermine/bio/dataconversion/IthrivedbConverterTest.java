bfd83155e150cb93e94aebe3c33ec80cdd3db6c5
sergiocontrino <contrino.it@gmail.com>
2021-06-15 09:17:32 +0100

    fixes imports

--- ithrivedb/src/test/java/org/intermine/bio/dataconversion/IthrivedbConverterTest.java
+++ ithrivedb/src/test/java/org/intermine/bio/dataconversion/IthrivedbConverterTest.java
@@ -10,6 +10,10 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import org.intermine.dataconversion.ItemsTestCase;
+import org.intermine.dataconversion.MockItemWriter;
+import org.intermine.metadata.Model;
+
 public class IthrivedbConverterTest extends ItemsTestCase
 {
     Model model = Model.getInstanceByName("genomic");
